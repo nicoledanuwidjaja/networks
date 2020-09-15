6dba492d8dea47a868eda87632f8b00d405e8f1e
Nicole Danuwidjaja <nicole-danuwidjaja@zwift.com>
2020-09-15 01:03:23 -0400

    more updates for reflection, command line arguments, and secret_flags file

--- pset1/client.py
+++ pset1/client.py
@@ -1,20 +1,39 @@
 import socket
 import sys
 
-host = '3700.network'
-port = 27993
-address = (host, port)
+# default values
+NUID = '001489806'
+HOST = '3700.network'
+PORT = 27993
+
+# Usage: ./client <-p port> <-s> [hostname] [NEU ID]
+args = sys.argv
+port_flag = '-p' in args
+ssl_flag = '-s' in args
+
+# check if optional -p flag is properly set up or not used
+valid_port = (port_flag and args[port_flag + 1].isdigit()) or not port_flag
+
+if len(args) < 6 and args[-1].isdigit() and valid_port:
+    if port_flag:
+        PORT = int(args[port_flag + 1])
+    HOST = args[-2]
+    NUID = args[-1]
+else:
+    print('Usage: ./client <-p port> <-s> [hostname] [NEU ID]')
+    sys.exit(1)
+
+address = (HOST, PORT)
 
 # creates TCP/IP socket
 # sockets enable data transfer with the Internet via API
 # AF_INET = IPv4 address
 # SOCK_STREAM = stream sockets
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket connected to %s on port %s' % address)
 
 try:
     s.connect(address)
-except OSError as msg:
+except OSError:
     s.close()
     s = None
 
@@ -23,23 +42,23 @@ if s is None:
     sys.exit(1)
 
 # obtain information from server and respond to messages
-HELLO = b'cs3700fall2020 HELLO 001489806\n'
-s.sendall(HELLO)
+HELLO = 'cs3700fall2020 HELLO ' + NUID + '\n'
+s.sendall(bytes(HELLO))
 
 
 def count_keys():
     while True:
         raw_data = ''
-        msg = s.recv(8192).split()
+        msg = str(s.recv(8192)).split()
 
+        # close socket connection
         if msg[1] == 'BYE':
-            bye = msg
             s.close()
-            return bye
+            return msg
 
         key = msg[2]
         jumbalaya = msg[3]
-        print(jumbalaya)
+        print(msg)
         raw_data += jumbalaya
 
         # read data until \n
@@ -48,13 +67,13 @@ def count_keys():
             raw_data += jumbalaya
 
         print("CURRENT KEY: %s " % key)
-        print(raw_data)
+        print("BEGIN %s" % msg)
         key_count = str(raw_data.count(str(key)))
 
         COUNT = 'cs3700fall2020 COUNT ' + key_count + '\n'
         s.sendall(bytes(COUNT))
-        print(bytes(COUNT))
+        print(COUNT)
 
 
-# f2c8b1629cec0f3d145ca192a9921a026574498378500bf60190c9168f1b9635
-flag = count_keys().split()[2]
+flag = count_keys()[2]
+print(flag)
